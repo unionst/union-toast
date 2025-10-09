dab64c6ca9175568c4fca1a618bc0054a159df26
ElhaDev <ben.s.elhadad@gmail.com>
2025-10-09 12:38:00 -0400

    remove unnecessary block

--- iOS26NotifView/iOS26NotifView/iOS26NotifView.swift
+++ iOS26NotifView/iOS26NotifView/iOS26NotifView.swift
@@ -29,10 +29,6 @@ struct iOS26NotificationBackground: View {
                     lineWidth: 1.3
                 )
                 .blendMode(.overlay)
-            
-            RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
-                        .strokeBorder(Color.white.opacity(0.1), lineWidth: 1)
-                        .shadow(color: Color.black.opacity(0.25), radius: 10, x: 0, y: 5)
         }
         .frame(width: width, height: height)
     }
